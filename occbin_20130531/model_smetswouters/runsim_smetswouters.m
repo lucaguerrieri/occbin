e1b41fa8e75c1f957bf9ef7b25647caafbf44989
Luca Guerrieri <luca.guerrieri@frb.gov>
2013-06-06 21:26:23 -0400

    Cleaned up comments

--- occbin_20130531/model_smetswouters/runsim_smetswouters.m
+++ occbin_20130531/model_smetswouters/runsim_smetswouters.m
@@ -10,17 +10,12 @@ clear
 
 global M_ oo_
 
-% modname below chooses model
-% directory. But simple param choices are made from paramfile in current
-% directory.
+% modnam and modnamstar below choose model
 modnam = 'usmodel';
 modnamstar = 'usmodelzlb';
 
 
-% express the occasionally binding constraint
-% in linearized form
-% one can use any combination of endogenous variables and parameters 
-% declared in the the dynare .mod files
+% see notes 1 and 2 in readme file
 constraint = 'r<-conster'; 
 constraint_relax ='rnot>-conster';
 
@@ -31,11 +26,7 @@ irfshock =char('eb');      % label for innovation for IRFs
                              % dynare .mod files
 
 
-
-
-
-
-
+% Shock path
   nper=10;
   
   shockssequence = [
@@ -45,9 +36,10 @@ irfshock =char('eb');      % label for innovation for IRFs
 
   
 
+%% Solve model
 
-tic
 % Solve model, generate model IRFs
+tic
 [zdatalinear zdatapiecewise zdatass oobase_ Mbase_  ] = ...
   solve_one_constraint(modnam,modnamstar,...
   constraint, constraint_relax,...
@@ -67,10 +59,13 @@ for i=1:Mbase_.param_nbr
     eval([Mbase_.param_names(i,:),'=Mbase_.params(i);'])
 end
 
-%% Modify to plot IRFs and decision rules
+%% Modify to plot IRFs 
 
-titlelist = char('r, Policy Interest Rate (percent at AR)','c, Consumption','inve, Investment','y, Output','b, preference shock');
-percent = 'PPt dev. from Steady State';
+titlelist = char('r, Policy Interest Rate (percent at AR)','c, Consumption',...
+                 'inve, Investment','y, Output', ...
+                 'b, preference shock');
+
+percent = 'Percent dev. from Steady State';
 level = 'Level';
 ylabels = char(level,percent,percent,percent,percent);
 
@@ -78,7 +73,6 @@ figtitle = '';
 line1=[4*(r_difference+conster),c_difference,inve_difference,y_difference,b_difference];
 line2=[4*(r_uncdifference+conster),c_uncdifference,inve_uncdifference,y_uncdifference,b_uncdifference];
 
-% Figure 1
 legendlist = cellstr(char('Piecewise Linear','Linear (ignores ZLB)'));
 figlabel = '';
 makechart(titlelist,legendlist,figlabel,ylabels,line1,line2)
