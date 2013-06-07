b5382e4d5b1d277e1339bac80ff9fdb5e3564143
Luca Guerrieri <luca.guerrieri@frb.gov>
2013-06-06 21:15:11 -0400

    removed mention of private_toolkit from paths

--- occbin_20130531/setpathdynare4.m
+++ occbin_20130531/setpathdynare4.m
@@ -1,8 +1,8 @@
 % uncomment a location below -- adapt to local installation
 
 % location = 'home_matteo';
-% location = 'home_luca';
-location = 'work_matteo';
+location = 'home_luca';
+% location = 'work_matteo';
 
 restoredefaultpath
 
@@ -12,19 +12,19 @@ if strmatch(location,'home_matteo','exact')
     dir3='C:\E\occbin\occbin_20130531\toolkit_files';
     rmpath 'C:\Program Files (x86)\MATLAB\R2009b\toolbox\ident\idobsolete'
 elseif strmatch(location,'home_luca','exact')
-    dir1='/users/jason/documents/matlab/dynare/4.1.0/matlab';
-    dir2='../toolkit_files';
+    dir1='/Applications/dynare/4.3.3/matlab';
+    dir2='/Users/Jason/Documents/occbin_repository/occbin_20130531/toolkit_files';
 elseif strmatch(location,'work_matteo','exact')  
     dir1='C:\E\dynare\4.3.1\matlab';
-    dir2='C:\E\occbin\occbin_20130531\toolkit_files_private';
-    dir3='C:\E\occbin\occbin_20130531\toolkit_files';
+    dir2='C:\E\occbin\occbin_20130531\toolkit_files';
+    
 else 
     error('Specify path to Dynare installation')
 end
 
 path(dir1,path);
 path(dir2,path);
-path(dir3,path);
+
 
 dynare_config
 
